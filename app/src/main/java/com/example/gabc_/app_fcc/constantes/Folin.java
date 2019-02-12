b52828b5402c48a3ec7d1dde634bde4455bf1201
HP Elitebook <gabc_arsenalero@live.cl>
2019-02-12 19:00:01 -0300

    añadido interfaces

---
+++ app/src/main/java/com/example/gabc_/app_fcc/constantes/Folin.java
@@ -0,0 +1,115 @@
+package com.example.gabc_.app_fcc.constantes;
+
+public interface Folin {
+
+    int FOLIN_RED_MDMA = 8;
+    int FOLIN_GREEN_MDMA = 8;
+    int FOLIN_BLUE_MDMA = 10;
+    String FOLIN_SMDMA = "MDMA";
+
+    int FOLIN_RED_MDE = 8;
+    int FOLIN_GREEN_MDE = 8;
+    int FOLIN_BLUE_MDE = 10;
+    String FOLIN_SMDE = "MDE";
+
+    int FOLIN_RED_SASS = 8;
+    int FOLIN_GREEN_SASS = 8;
+    int FOLIN_BLUE_SASS = 10;
+    String FOLIN_SSASS = "MDA (Sass)";
+
+    int FOLIN_RED_5APB = 8;
+    int FOLIN_GREEN_5APB = 8;
+    int FOLIN_BLUE_5APB = 10;
+    String FOLIN_S5APB = "5-APB";
+
+    int FOLIN_RED_6APB = 8;
+    int FOLIN_GREEN_6APB = 8;
+    int FOLIN_BLUE_6APB = 10;
+    String FOLIN_S6APB = "6-APB";
+
+    int FOLIN_RED_DXM = 8;
+    int FOLIN_GREEN_DXM = 8;
+    int FOLIN_BLUE_DXM = 10;
+    String FOLIN_SDXM = "DXM";
+
+    int FOLIN_RED_ETHYLONE = 8;
+    int FOLIN_GREEN_ETHYLONE = 8;
+    int FOLIN_BLUE_ETHYLONE = 10;
+    String FOLIN_SETHYLONE = "Ethylone";
+
+    int FOLIN_RED_SUGAR = 8;
+    int FOLIN_GREEN_SUGAR = 8;
+    int FOLIN_BLUE_SUGAR = 10;
+    String FOLIN_SSUGAR = "Sugar";
+
+    int FOLIN_RED_METHYLONE = 244;
+    int FOLIN_GREEN_METHYLONE = 238;
+    int FOLIN_BLUE_METHYLONE = 80;
+    String FOLIN_SMETHYLONE = "Methylone";
+
+    int FOLIN_RED_BUTYLONE = 244;
+    int FOLIN_GREEN_BUTYLONE = 238;
+    int FOLIN_BLUE_BUTYLONE = 80;
+    String FOLIN_SBUTYLONE = "Butylone";
+
+    int FOLIN_RED_MDPV = 244;
+    int FOLIN_GREEN_MDPV = 238;
+    int FOLIN_BLUE_MDPVE = 80;
+    String FOLIN_SMDPV = "MDPV";
+
+    int FOLIN_RED_METANPHETAMINE= 223;
+    int  FOLIN_GREEN_METANPHETAMINE = 113;
+    int FOLIN_BLUE_METANPHETAMINE = 44;
+    String FOLIN_SMETAMPHETAMINE = "Metamphetamine / Amphetamine / Mescaline";
+
+    int FOLIN_RED_AMPHETAMINE= 223;
+    int  FOLIN_GREEN_AMPHETAMINE = 113;
+    int FOLIN_BLUE_AMPHETAMINE = 44;
+    String FOLIN_SAMPHETAMINE = "Metamphetamine / Amphetamine / Mescaline";
+
+    int FOLIN_RED_MESCALINE= 223;
+    int  FOLIN_GREEN_MESCALINE = 113;
+    int FOLIN_BLUE_MESCALINE = 44;
+    String FOLIN_S3 = "Metamphetamine / Amphetamine / Mescaline";
+
+    int FOLIN_RED_CODEYNE = 184;
+    int FOLIN_GREEN_CODEYNE = 156;
+    int FOLIN_BLUE_CODEYNE = 195;
+    String FOLIN_SCODEYNE = "Codeyne";
+
+    int FOLIN_RED_MORPHINE = 184;
+    int FOLIN_GREEN_MORPHINE = 156;
+    int FOLIN_BLUE_MORPHINE = 195;
+    String FOLIN_SMORPHINE = "Morphine";
+
+    int FOLIN_RED_OXYCODONE = 184;
+    int FOLIN_GREEN_OXYCODONE = 156;
+    int FOLIN_BLUE_OXYCODONE = 195;
+    String FOLIN_SOXYCODONE = "Oxycodone";
+
+    int FOLIN_RED_ACB = 18;
+    int FOLIN_GREEN_ACB = 83;
+    int FOLIN_BLUE_ACB = 48;
+    String FOLIN_S5 = "aC-B";
+
+    int FOLIN_RED_AC1 = 37;
+    int FOLIN_GREEN_AC1 = 70;
+    int FOLIN_BLUE_AC1 = 120;
+    String FOLIN_SAC1 = "aC-I";
+
+    int FOLIN_RED_HEROIN = 143;
+    int FOLIN_GREEN_HEROIN = 27;
+    int FOLIN_BLUE_HEROIN = 38;
+    String FOLIN_SHEROIN = "Heroin";
+
+    int FOLIN_RED_ASPIRIN = 236;
+    int FOLIN_GREEN_ASPIRIN = 43;
+    int FOLIN_BLUE_ASPIRIN = 51;
+    String FOLIN_SASPIRIN = "Aspirin";
+
+    int FOLIN_REDNR = 180;
+    int FOLIN_GREENNR = 151;
+    int FOLIN_BLUENR = 120;
+    String FOLIN_NR = "SIN REACCIÓN";
+
+}
---
+++ app/src/main/java/com/example/gabc_/app_fcc/constantes/Froehde.java
@@ -0,0 +1,115 @@
+package com.example.gabc_.app_fcc.constantes;
+
+public interface Froehde {
+
+    int FROEHDE_RED_MDMA = 8;
+    int FROEHDE_GREEN_MDMA = 8;
+    int FROEHDE_BLUE_MDMA = 10;
+    String FROEHDE_SMDMA = "MDMA";
+
+    int FROEHDE_RED_MDE = 8;
+    int FROEHDE_GREEN_MDE = 8;
+    int FROEHDE_BLUE_MDE = 10;
+    String FROEHDE_SMDE = "MDE";
+
+    int FROEHDE_RED_SASS = 8;
+    int FROEHDE_GREEN_SASS = 8;
+    int FROEHDE_BLUE_SASS = 10;
+    String FROEHDE_SSASS = "MDA (Sass)";
+
+    int FROEHDE_RED_5APB = 8;
+    int FROEHDE_GREEN_5APB = 8;
+    int FROEHDE_BLUE_5APB = 10;
+    String FROEHDE_S5APB = "5-APB";
+
+    int FROEHDE_RED_6APB = 8;
+    int FROEHDE_GREEN_6APB = 8;
+    int FROEHDE_BLUE_6APB = 10;
+    String FROEHDE_S6APB = "6-APB";
+
+    int FROEHDE_RED_DXM = 8;
+    int FROEHDE_GREEN_DXM = 8;
+    int FROEHDE_BLUE_DXM = 10;
+    String FROEHDE_SDXM = "DXM";
+
+    int FROEHDE_RED_ETHYLONE = 8;
+    int FROEHDE_GREEN_ETHYLONE = 8;
+    int FROEHDE_BLUE_ETHYLONE = 10;
+    String FROEHDE_SETHYLONE = "Ethylone";
+
+    int FROEHDE_RED_SUGAR = 8;
+    int FROEHDE_GREEN_SUGAR = 8;
+    int FROEHDE_BLUE_SUGAR = 10;
+    String FROEHDE_SSUGAR = "Sugar";
+
+    int FROEHDE_RED_METHYLONE = 244;
+    int FROEHDE_GREEN_METHYLONE = 238;
+    int FROEHDE_BLUE_METHYLONE = 80;
+    String FROEHDE_SMETHYLONE = "Methylone";
+
+    int FROEHDE_RED_BUTYLONE = 244;
+    int FROEHDE_GREEN_BUTYLONE = 238;
+    int FROEHDE_BLUE_BUTYLONE = 80;
+    String FROEHDE_SBUTYLONE = "Butylone";
+
+    int FROEHDE_RED_MDPV = 244;
+    int FROEHDE_GREEN_MDPV = 238;
+    int FROEHDE_BLUE_MDPVE = 80;
+    String FROEHDE_SMDPV = "MDPV";
+
+    int FROEHDE_RED_METANPHETAMINE= 223;
+    int  FROEHDE_GREEN_METANPHETAMINE = 113;
+    int FROEHDE_BLUE_METANPHETAMINE = 44;
+    String FROEHDE_SMETAMPHETAMINE = "Metamphetamine / Amphetamine / Mescaline";
+
+    int FROEHDE_RED_AMPHETAMINE= 223;
+    int  FROEHDE_GREEN_AMPHETAMINE = 113;
+    int FROEHDE_BLUE_AMPHETAMINE = 44;
+    String FROEHDE_SAMPHETAMINE = "Metamphetamine / Amphetamine / Mescaline";
+
+    int FROEHDE_RED_MESCALINE= 223;
+    int  FROEHDE_GREEN_MESCALINE = 113;
+    int FROEHDE_BLUE_MESCALINE = 44;
+    String FROEHDE_S3 = "Metamphetamine / Amphetamine / Mescaline";
+
+    int FROEHDE_RED_CODEYNE = 184;
+    int FROEHDE_GREEN_CODEYNE = 156;
+    int FROEHDE_BLUE_CODEYNE = 195;
+    String FROEHDE_SCODEYNE = "Codeyne";
+
+    int FROEHDE_RED_MORPHINE = 184;
+    int FROEHDE_GREEN_MORPHINE = 156;
+    int FROEHDE_BLUE_MORPHINE = 195;
+    String FROEHDE_SMORPHINE = "Morphine";
+
+    int FROEHDE_RED_OXYCODONE = 184;
+    int FROEHDE_GREEN_OXYCODONE = 156;
+    int FROEHDE_BLUE_OXYCODONE = 195;
+    String FROEHDE_SOXYCODONE = "Oxycodone";
+
+    int FROEHDE_RED_ACB = 18;
+    int FROEHDE_GREEN_ACB = 83;
+    int FROEHDE_BLUE_ACB = 48;
+    String FROEHDE_S5 = "aC-B";
+
+    int FROEHDE_RED_AC1 = 37;
+    int FROEHDE_GREEN_AC1 = 70;
+    int FROEHDE_BLUE_AC1 = 120;
+    String FROEHDE_SAC1 = "aC-I";
+
+    int FROEHDE_RED_HEROIN = 143;
+    int FROEHDE_GREEN_HEROIN = 27;
+    int FROEHDE_BLUE_HEROIN = 38;
+    String FROEHDE_SHEROIN = "Heroin";
+
+    int FROEHDE_RED_ASPIRIN = 236;
+    int FROEHDE_GREEN_ASPIRIN = 43;
+    int FROEHDE_BLUE_ASPIRIN = 51;
+    String FROEHDE_SASPIRIN = "Aspirin";
+
+    int FROEHDE_REDNR = 180;
+    int FROEHDE_GREENNR = 151;
+    int FROEHDE_BLUENR = 120;
+    String FROEHDE_NR = "SIN REACCIÓN";
+
+}
