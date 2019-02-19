759a90c37544f19d65d7a5ccf443fd04e69b5e21
Gerard <gabc_arsenalero@live.cl>
2019-02-18 21:06:54 -0300

    Cuarto commit

--- app/src/main/java/com/example/gabc_/app_fcc/RegistroMuestraActivity.java
+++ app/src/main/java/com/example/gabc_/app_fcc/RegistroMuestraActivity.java
@@ -4,13 +4,19 @@ import android.annotation.SuppressLint;
 import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.TextureView;
-import android.widget.RadioGroup;
 import android.widget.TextView;
 
-public class RegistroMuestraActivity extends AppCompatActivity implements Constantes {
+import com.example.gabc_.app_fcc.constantes.Folin;
+import com.example.gabc_.app_fcc.constantes.Froehde;
+import com.example.gabc_.app_fcc.constantes.Liebermann;
+import com.example.gabc_.app_fcc.constantes.Mandelin;
+import com.example.gabc_.app_fcc.constantes.Marckis;
+import com.example.gabc_.app_fcc.constantes.Mecke;
+import com.example.gabc_.app_fcc.constantes.Simon;
 
-    private TextView rgb,hex,che,r1,g1,b1,r2,g2,b2,color1tv;
+public class RegistroMuestraActivity extends AppCompatActivity implements Marckis, Mecke, Mandelin, Simon, Liebermann, Froehde, Folin {
+
+    private TextView rgb,hex,che,r1,g1,b1,sust1,color1tv,rgb2,hex2,che2;
 
     @SuppressLint("SetTextI18n")
     @Override
@@ -22,32 +28,49 @@ public class RegistroMuestraActivity extends AppCompatActivity implements Consta
         hex = findViewById(R.id.hex_tv);
         che = findViewById(R.id.chemical_tv);
 
-        r1 = findViewById(R.id.red1);
-        r2 = findViewById(R.id.red2);
-        g1 = findViewById(R.id.green1);
-        g2 = findViewById(R.id.green2);
-        b1 = findViewById(R.id.blue1);
-        b2 = findViewById(R.id.blue2);
+        rgb2 = findViewById(R.id.rgb_tv2);
+        hex2 = findViewById(R.id.hex_tv2);
+        che2 = findViewById(R.id.chemical_tv2);
 
-        color1tv = findViewById(R.id.color1);
+        r1 = findViewById(R.id.red1);
+        g1 = findViewById(R.id.green1);
+        b1 = findViewById(R.id.blue1);
+
+        /*r1 = findViewById(R.id.red2);
+        g1 = findViewById(R.id.green2);
+        b1 = findViewById(R.id.blue2);*/
+
+        color1tv = findViewById(R.id.sustancia);
 
         Bundle objeto = this.getIntent().getExtras();
         String rgb_tomamuestra = objeto.getString("rgb_tm");
         String hex_tomamuestra = objeto.getString("hex_tm");
         String che_tomamuestra = objeto.getString("che_tm");
+        String rgb_tomamuestra2 = objeto.getString("rgb_tm2");
+        String hex_tomamuestra2 = objeto.getString("hex_tm2");
+        String che_tomamuestra2 = objeto.getString("che_tm2");
         rgb.setText(rgb_tomamuestra);
         hex.setText(hex_tomamuestra);
         che.setText(che_tomamuestra);
 
-        r1.setText("R "+String.valueOf(MARCKIS_B1));
-        g1.setText("G "+String.valueOf(MARCKIS_G1));
-        b1.setText("B "+String.valueOf(MARCKIS_B1));
+        rgb2.setText(rgb_tomamuestra2);
+        hex2.setText(hex_tomamuestra2);
+        che2.setText(che_tomamuestra2);
 
-        int rojo1 = Color.red(MARCKIS_R1);
-        int verde1 = Color.red(MARCKIS_G1);
-        int azul1 = Color.red(MARCKIS_B1);
+        int resta_red1, red1;
+        String sustancia_coincide;
 
-        color1tv.setBackgroundColor(Color.rgb(MARCKIS_R1,MARCKIS_G1,MARCKIS_B1));
+
+
+        r1.setText("R "+String.valueOf(MARCKIS_RED_MDMA));
+        g1.setText("G "+String.valueOf(MARCKIS_GREEN_MDMA));
+        b1.setText("B "+String.valueOf(MARCKIS_BLUE_MDMA));
+
+        int rojo1 = Color.red(MARCKIS_RED_MDMA);
+        int verde1 = Color.red(MARCKIS_GREEN_MDMA);
+        int azul1 = Color.red(MARCKIS_BLUE_MDMA);
+
+        color1tv.setText(MARCKIS_SMDMA);
 
     }
 }
--- app/src/main/java/com/example/gabc_/app_fcc/TomaMuestraActivity.java
+++ app/src/main/java/com/example/gabc_/app_fcc/TomaMuestraActivity.java
@@ -24,7 +24,8 @@ import java.net.URL;
 public class TomaMuestraActivity extends AppCompatActivity {
 
     private ImageView imageView;
-    private TextView textView, hexView, color, tvQuimico;
+    private TextView textView, hexView, color, tvQuimico, textView2, hexView2, color2, tvQuimico2, cancelar,
+            textView3, textView4, hexView3, hexView4, color3, color4, tvQuimico3, tvQuimico4;
     private Bitmap bitmap;
     private Button registro;
     private RadioGroup quimico;
@@ -36,16 +37,38 @@ public class TomaMuestraActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_toma_muestra);
 
+        cancelar = findViewById(R.id.btn_cancelar);
         imageView = findViewById(R.id.imgImagen);
         textView = findViewById(R.id.txtRGB);
         hexView = findViewById(R.id.txtHexa);
         color = findViewById(R.id.txtColor);
         registro = findViewById(R.id.btn_registro);
-        quimico = findViewById(R.id.rg_quimicos);
+        quimico = findViewById(R.id.rgQuimico);
         tvQuimico = findViewById(R.id.txtQuimico);
+        textView2 = findViewById(R.id.txtRGB2);
+        hexView2 = findViewById(R.id.txtHexa2);
+        color2 = findViewById(R.id.txtColor2);
+        tvQuimico2 = findViewById(R.id.txtQuimico2);
+        textView3 = findViewById(R.id.txtRGB3);
+        hexView3 = findViewById(R.id.txtHexa3);
+        color3 = findViewById(R.id.txtColor3);
+        tvQuimico3 = findViewById(R.id.txtQuimico3);
+        textView4 = findViewById(R.id.txtRGB4);
+        hexView4 = findViewById(R.id.txtHexa4);
+        color4 = findViewById(R.id.txtColor4);
+        tvQuimico4 = findViewById(R.id.txtQuimico4);
 
         imageView.setDrawingCacheEnabled(true);
         imageView.buildDrawingCache(true);
+
+        cancelar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(TomaMuestraActivity.this , MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
 
         imageView.setOnTouchListener(new View.OnTouchListener() {
             @Override
@@ -60,12 +83,38 @@ public class TomaMuestraActivity extends AppCompatActivity {
                     int g = Color.green(pixel);
                     int b = Color.blue(pixel);
 
+                    int r2 = Color.red(pixel);
+                    int g2 = Color.green(pixel);
+                    int b2 = Color.blue(pixel);
+
+                    int r3 = Color.red(pixel);
+                    int g3 = Color.green(pixel);
+                    int b3 = Color.blue(pixel);
+
+                    int r4 = Color.red(pixel);
+                    int g4 = Color.green(pixel);
+                    int b4 = Color.blue(pixel);
+
                     //textView.setBackgroundColor(Color.rgb(r,g,b));
                     textView.setText("("+r+","+g+","+b+")");
+                    textView2.setText("("+r2+","+g2+","+b2+")");
+                    textView3.setText("("+r3+","+g3+","+b3+")");
+                    textView4.setText("("+r4+","+g4+","+b4+")");
 
                     String c = ColorHex(r,g,b);
+                    String c2 = ColorHex(r2,g2,b2);
+                    String c3 = ColorHex(r3,g3,b3);
+                    String c4 = ColorHex(r4,g4,b4);
+
                     hexView.setText(c);
+                    hexView2.setText(c2);
+                    hexView3.setText(c3);
+                    hexView4.setText(c4);
+
                     color.setBackgroundColor(android.graphics.Color.rgb(r,g,b));
+                    color2.setBackgroundColor(android.graphics.Color.rgb(r2,g2,b2));
+                    color3.setBackgroundColor(android.graphics.Color.rgb(r3,g3,b3));
+                    color4.setBackgroundColor(android.graphics.Color.rgb(r4,g4,b4));
                 }
 
                 return true;
@@ -79,9 +128,14 @@ public class TomaMuestraActivity extends AppCompatActivity {
                 intent.putExtra("rgb_tm", textView.getText());
                 intent.putExtra("hex_tm", hexView.getText());
                 intent.putExtra("che_tm", tvQuimico.getText());
+                intent.putExtra("rgb_tm2", textView2.getText());
+                intent.putExtra("hex_tm2", hexView2.getText());
+                intent.putExtra("che_tm2", tvQuimico2.getText());
                 startActivity(intent);
             }
         });
+
+
 
     }
 
